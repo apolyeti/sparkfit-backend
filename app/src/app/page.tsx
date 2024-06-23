478dae3c06a422de41939eac8905fdbe014b2083
apolyeti <azhandarveen@gmail.com>
2024-06-22 21:22:15 -0700

    feat: image reading

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,14 +1,42 @@
 "use client";
-import Image from "next/image";
+import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import Image from "next/image";
 
 export default function Home() {
-    const { getRootProps, getInputProps } = useDropzone();
+
+    const [image, setImage] = useState<string | null>(null);
+
+    const onDrop = useCallback((acceptedFiles : File[]) => {
+        acceptedFiles.forEach((file) => {
+            const reader = new FileReader();
+
+            reader.onabort = () => console.log("file reading was aborted");
+            reader.onerror = () => console.log("file reading has failed");
+
+            reader.onload = () => {
+                // save the image to images/ folder
+                setImage(reader.result as string);
+            }
+            reader.readAsDataURL(file);
+        })
+    }, []);
+
+    const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
     return (
         <div {...getRootProps()}>
             <input {...getInputProps()} />
             <p>Upload photo here</p>
+
+            <div id="imageDisplay">
+                {image && 
+                    <Image 
+                        src={image} alt="uploaded image" 
+                        width={200} height={200}
+                    />
+                }
+            </div>
         </div>
     );
 }
