9e01c5a048d7ba7c07540d316794e81216e3ee66
apolyeti <azhandarveen@gmail.com>
2024-07-02 14:31:44 -0700

    refactor: restart identifyClothing api endpoint

--- app/src/app/api/identifyClothing/route.ts
+++ app/src/app/api/identifyClothing/route.ts
@@ -1,32 +1,10 @@
+// import tensorflowjs-node
 import * as tf from '@tensorflow/tfjs-node';
-import {  NextRequest,  NextResponse } from 'next/server';
-import * as path from 'path';
 import * as fs from 'fs';
+import {NextRequest, NextResponse} from 'next/server';
 
-
-// function to get class names from directory
-function getClassNamesFromDirectory(directory: string) {
-    // each class name is the name of a subdirectory in the directory
-    const classNames = fs.readdirSync(directory);
-    return classNames;
-}
-
-// make post request to server
-export async function POST(req: NextRequest) {
-    console.log('hi')
-    // get image from request body
+export async function POST(req: NextRequest, res: NextResponse) {
     const body = await req.json();
-    console.log("HELLO WE GOT HERE");
-
-    // download image
-    const base64Image = body.image;
-    const fileName = body.fileName;
-    const imageBuffer = Buffer.from(base64Image, 'base64');
-    fs.writeFileSync('src/app/api/identifyClothing/image.jpg', imageBuffer);
-
-
-
-
-    
-    return NextResponse.json({ name: fileName });
+    console.log(body);
+    return NextResponse.json({ name: body.name });
 }
