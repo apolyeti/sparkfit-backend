898c8c345051bdb910f43c81105234652fa11a52
apolyeti <azhandarveen@gmail.com>
2024-07-02 15:31:17 -0700

    feat: make sure api/identifyClothing returns a response for now

--- app/src/app/api/identifyClothing/route.ts
+++ app/src/app/api/identifyClothing/route.ts
@@ -5,12 +5,12 @@ import {NextRequest, NextResponse} from 'next/server';
 
 export async function POST(req: NextRequest, res: NextResponse) {
     const body = await req.json();
-    const image = body.image;
+    // const image = body.image;
 
-    // process the image first
+    // // process the image first
     
-    // load the model
-    const model = await tf.loadLayersModel('file://path/to/model.json');
+    // // load the model
+    // const model = await tf.loadLayersModel('file://path/to/model.json');
 
-    return NextResponse.json({ name: 'name' });
+    // return NextResponse.json({ name: 'name' });
 }
