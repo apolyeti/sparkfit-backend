dba306fd6b407f748669b73e3b3eac9298185146
apolyeti <azhandarveen@gmail.com>
2024-07-03 16:36:36 -0700

    feat: working with tailwindcss styles now

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -10,9 +10,13 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div>
-      <h1>Sparkfit</h1>
-      <p>Get outfit suggestions based on the weather</p>
-    </div>    
+    <>
+        <h1 className="text-4xl font-bold">
+            Sparkfit
+        </h1>
+        {/* add spacer here */}
+        <span className="block h-4"></span>
+        <p>Get outfit suggestions based on the weather</p>
+    </>
   );
 }
