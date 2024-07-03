544143121fee66885f80518ab01f5a11ecb84a2f
apolyeti <azhandarveen@gmail.com>
2024-07-03 15:50:23 -0700

    chore: write metadata for root page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,12 @@
 import Image from "next/image";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Sparkfit",
+    description: "Get outfit suggestions based on the weather",
+}
+
+
 
 export default function Home() {
   return (
