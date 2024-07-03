a694294b7683d70bdd7f5026cc5831baff3d23e6
apolyeti <azhandarveen@gmail.com>
2024-07-02 17:23:18 -0700

    feat: need to figure out how to allow nextjs to handle tensors

--- app/src/app/api/identifyClothing/route.ts
+++ app/src/app/api/identifyClothing/route.ts
@@ -8,6 +8,40 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     const image = formData.get('image') as File;
     const name = formData.get('name') as string;
+    console.log(name);
 
-    return NextResponse.json({name: name});
+    const buffer = await image.arrayBuffer();
+    // const tensor = tf.node.decodeImage(new Uint8Array(buffer));
+//     // make the image 240x240
+//     const resized = tf.image.resizeBilinear(tensor, [240, 240]);
+//     const expanded = resized.expandDims(0);
+
+    const modelPath = 
+        'file://../tools/models/classify_clothes/classify_clothes.keras';
+    // load the model
+    // const model = await tf.loadLayersModel(modelPath);
+
+    // // predict the image
+    // const prediction = model.predict(expanded) as tf.Tensor;
+
+    // const predictionData = prediction.dataSync();
+    // const maxIndex = predictionData.indexOf(Math.max(...predictionData));
+
+    // // get the label
+    // const labels = fs.readFileSync(
+    //     '../tools/models/classify_clothes/labels.txt', 'utf8');
+    // const labelArray = labels.split('\n');
+    // const label = labelArray[maxIndex];
+    // console.log('predicted label:', label)
+
+    const responseBody = {
+        name: name
+    };
+    // responseData.append('label', label);
+
+
+
+    return NextResponse.json(responseBody, {
+        status: 200
+    });
 }
