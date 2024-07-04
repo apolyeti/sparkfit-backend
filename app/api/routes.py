e18c61e7ccc8f3886164f140395ea0593ee190cb
apolyeti <azhandarveen@gmail.com>
2024-07-03 18:10:45 -0700

    feat: setting up flask for server side api routes

---
+++ app/api/routes.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, jsonify
+
+api = Blueprint('api', __name__)
+
+
+
+
+@api.route('/classifyClothing', methods=['POST'])
+def classify_clothing():
+    """
+    /api/classifyClothing
+    ---
+    tags:
+      - Clothing Classification
+    description: Classify clothing using a trained model
+    parameters:
+        -   name: name
+            in: query
+            type: string
+            required: true
+            description: The name of the image file
+        -   name: image
+            in: query
+            type: file
+            required: true
+            description: The image file to classify
+    responses:
+        200:
+            description: Clothing classification successful
+        400:
+            description: Bad request
+        500:
+            description: Internal server error
+    """
+    return jsonify({'message': 'Classifying clothing...'})
