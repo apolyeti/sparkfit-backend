274ba5cb6102ba0b970908d331356e730b58fb14
apolyeti <azhandarveen@gmail.com>
2024-07-09 14:11:14 -0700

    feat: create a helper for localStorage checking

---
+++ client/src/utils/helpers.ts
@@ -0,0 +1,19 @@
+function storageAvailable(type: 'localStorage' | 'sessionStorage'): boolean {
+    let storage: Storage | null = null;
+    try {
+        storage = window[type as keyof Window] as Storage;
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return (
+        e instanceof DOMException &&
+        (e.name === "QuotaExceededError" ||
+            e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage !== null &&
+        storage.length !== 0
+        );
+    }
+}
