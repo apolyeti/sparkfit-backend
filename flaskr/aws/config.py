e00ad5dfc78c529e28b60e113904ed014a95273c
Arveen Azhand <azhandarveen@gmail.com>
2024-07-31 20:25:48 -0700

    docs: config.py and dynamo.py

--- flaskr/aws/config.py
+++ flaskr/aws/config.py
@@ -1,3 +1,16 @@
+"""
+This module contains the configuration for the AWS session using the boto3 AWS SDK.
+To use this module, set the following environment variables:
+    - AWS_ACCESS_KEY_ID: The AWS access key ID.
+    - AWS_SECRET_KEY_ID: The AWS secret access key.
+    - AWS_REGION: The AWS region name.
+For more information on AWS sessions, see https://boto3.amazonaws.com/v1/documentation/api/latest/guide/session.html
+
+Functions:
+    get_aws_session: Get an AWS session using the environment variables.
+
+"""
+
 import os
 
 import boto3
