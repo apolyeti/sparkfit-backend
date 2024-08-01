bba39b634270d48b25495f4e1e72720eb3e6a29b
Arveen Azhand <azhandarveen@gmail.com>
2024-07-31 20:17:15 -0700

    docs: config.py

--- flaskr/aws/config.py
+++ flaskr/aws/config.py
@@ -4,6 +4,10 @@ import boto3
 
 
 def get_aws_session():
+    """
+    Get an AWS session using the environment variables.
+    See https://boto3.amazonaws.com/v1/documentation/api/latest/guide/session.html
+    """
     _ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
     _SECRET_KEY = os.getenv("AWS_SECRET_KEY_ID")
     _REGION = os.getenv("AWS_REGION")
